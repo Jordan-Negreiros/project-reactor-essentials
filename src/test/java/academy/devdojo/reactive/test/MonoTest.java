ab6551ac61ee5195e4cfdb81d80d9dae8e83f41a
Jordan Negreiros <jordan.santos@picpay.com>
2021-10-27 19:36:18 -0300

    recap

--- src/test/java/academy/devdojo/reactive/test/MonoTest.java
+++ src/test/java/academy/devdojo/reactive/test/MonoTest.java
@@ -3,6 +3,21 @@ package academy.devdojo.reactive.test;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+/**
+ * Reative Streams
+ * 1.Asynchronous
+ * 2. Non-blocking
+ * 3. Backpressure
+ * Publisher <- (subscribe) Subscriber
+ * Subscription  is created
+ * Publisher (onSubscribe with the subscription) -> Subscriber
+ * Subscription <- (request N) Subscriber
+ * Publisher -> (onNext) Subscriber
+ * until:
+ * 1. Publisher sends all the objects requested.
+ * 2. Publisher sends all the objects it has. (onComplete) subscriber and subscrition will be canceled
+ * 3. There is an error. (onError) -> subscriber and subscrition will be canceled
+ */
 @Slf4j
 public class MonoTest {
 
